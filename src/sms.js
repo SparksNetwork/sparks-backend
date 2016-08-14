88061ed56a8f18dfcbee9ad176b2f0f20499e3af
sd <sdebaun74@gmail.com>
2016-08-13 20:57:27 -0700

    sms update

--- src/sms.js
+++ src/sms.js
@@ -7,7 +7,7 @@ async function call(row) {
   const [profileKey, name, email, phone, engKey, status, opp] = row
   console.log('calling', name, phone)
   const response = await twilio.messages.create({
-    body: 'HURRY! Confirm your Wild Woods volunteer spot or lose any shifts you selected. Confirm at http://sparks.network or email help@sparks.network to cancel and stop our texts.',
+    body: 'HURRY! Confirm your Dirtybird BBQ volunteer spot for tomorrow! Go to http://sparks.network to lock in your spot, or e-mail help@sparks.network to cancel and stop our texts.',
     to: phone,
     from: process.env.TWILIO_PHONE_NUMBER,
   })
