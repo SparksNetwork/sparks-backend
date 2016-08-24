73ffc503836ce358273e9f0102dab5a0081ff52d
sd <sdebaun74@gmail.com>
2016-08-24 11:50:15 -0700

    feat(sendgrid-v3): starting to implement Engagement plugin using new sendgrid plugin

---
+++ src/models/Engagements/src/index.test.ts
@@ -0,0 +1,121 @@
+import test from 'ava'
+import * as assert from 'assert'
+import {spy, stub} from 'sinon'
+import {stubs, SenecaWithPlugin} from '../../../testUtil'
+
+import {plugin} from './index'
+
+test('Engagement/create', async function(t) {
+  const seneca = await SenecaWithPlugin(plugin, {domain: 'http://sparks.network'})
+  t.plan(13)
+  // should generate a client token
+  seneca.add({role: 'gateway', cmd: 'generateClientToken'}, async function(args) {
+    t.truthy(true)
+    return {clientToken: 'CLIENT_TOKEN'}
+  })
+  // should push a new engagement to firebase
+  seneca.add({role: 'Firebase', cmd: 'push'}, function({model, values}) {
+    t.is(model, 'Engagements')
+    t.deepEqual(values, {
+      oppKey: 'OPP_KEY',
+      profileKey: 'PROFILE_KEY',
+      isApplied: true,
+      isAccepted: false,
+      isConfirmed: false,
+      paymentClientToken: 'CLIENT_TOKEN',
+    })
+    return {
+      key: 'ENGAGEMENT_KEY',
+    }
+  })
+  // should get info about engagement & related records
+  seneca.add({role: 'Firebase', cmd: 'get'}, async function({engagement, profile, opp, project}) {
+    t.is(engagement, 'ENGAGEMENT_KEY')
+    t.deepEqual(profile, ['engagement', 'profileKey'])
+    t.deepEqual(opp, ['engagement', 'oppKey'])
+    t.deepEqual(project, ['opp', 'projectKey'])
+    return {
+      engagement: {
+        profileKey: 'PROFILE_KEY',
+        oppKey: 'OPP_KEY',
+      },
+      profile: {
+        fullName: 'PROFILE_FULLNAME',
+        email: 'PROFILE_EMAIL',
+      },
+      opp: {
+        name: 'OPP_NAME',
+      },
+      project: {
+        name: 'PROJECT_NAME',
+      }
+    }
+  })
+  // should send an email
+  seneca.add({role: 'sendgrid', cmd: 'sendTemplate'}, async function(args) {
+    t.is(args.templateId, '96e36ab7-43b0-4d45-8309-32c52530bd8a')
+    t.is(args.subject, 'Sit Tight! PROJECT_NAME has your application.')
+    t.is(args.recipient, 'PROFILE_EMAIL')
+    t.is(args.sender, 'help@sparks.network')
+    t.deepEqual(args.substitutions, {
+      '-username-': 'PROFILE_FULLNAME',
+      '-opp_name-': 'OPP_NAME',
+      '-project_name-': 'PROJECT_NAME',
+      '-engagementurl-': 'http://sparks.network/engaged/ENGAGEMENT_KEY/',
+    })
+  })
+  const {key} = await seneca.act({
+    role: 'Engagements',
+    cmd: 'create',
+    oppKey: 'OPP_KEY',
+    profileKey: 'PROFILE_KEY',
+  })
+  // should return the newly created engagement key
+  t.is(key, 'ENGAGEMENT_KEY')
+})
+
+test.only('Engagement/remove', async function(t) {
+  const seneca = await SenecaWithPlugin(plugin, {domain: 'http://sparks.network'})
+  t.plan(8)
+  // should get assignments for the engagement
+  seneca.add({role: 'Firebase', cmd: 'get'}, async function({assignments}) {
+    t.deepEqual(assignments, {engagementKey: 'ENGAGEMENT_KEY'})
+    return {
+      assignments: [
+        {
+          $key: 'ASSIGNMENT_KEY_1',
+          shiftKey: 'SHIFT_KEY_1',
+        },
+        {
+          $key: 'ASSIGNMENT_KEY_2',
+          shiftKey: 'SHIFT_KEY_1',
+        },
+        {
+          $key: 'ASSIGNMENT_KEY_3',
+          shiftKey: 'SHIFT_KEY_2',
+        }
+      ],
+    }
+  })
+  // should issue a remove for each assignment
+  seneca.add({role: 'Firebase', cmd: 'remove', model: 'Assignments'}, async function({key}) {
+    t.truthy(['ASSIGNMENT_KEY_1', 'ASSIGNMENT_KEY_2', 'ASSIGNMENT_KEY_3'].indexOf(key) >= 0)
+    return {key}
+  })
+  // should issue a count refresh for each shift affected, but only once
+  seneca.add({role: 'Shifts', cmd: 'updateCounts'}, async function({key}) {
+    t.truthy(['SHIFT_KEY_1', 'SHIFT_KEY_2'].indexOf(key) >= 0)
+  })
+  // should issue a firebase remove for the engagement
+  seneca.add({role: 'Firebase', cmd: 'remove', model: 'Engagements'}, async function({key}) {
+    t.is(key, 'ENGAGEMENT_KEY')
+    return {key}
+  })
+  const {key} = await seneca.act({
+    role: 'Engagements',
+    cmd: 'remove',
+    key: 'ENGAGEMENT_KEY',
+  })
+  // should return the deleted key
+  t.is(key, 'ENGAGEMENT_KEY')
+})
---
+++ src/models/Engagements/src/index.ts
@@ -0,0 +1,109 @@
+import {compose, uniq, prop} from 'ramda'
+
+const ROLE = 'Engagements'
+
+export function plugin({domain}) {
+  const seneca = this
+  seneca.add({init: ROLE}, init({seneca, domain}))
+  return ROLE
+}
+
+function init({seneca, domain}) {
+  return async function(args) {
+    seneca.add({role: ROLE, cmd: 'create'}, create({seneca}))
+    seneca.add({role: ROLE, cmd: 'remove'}, remove({seneca}))
+    seneca.add({role: ROLE, cmd: 'notify'}, notify({seneca, domain}))
+  }
+}
+
+function create({seneca}) {
+  return async function({oppKey, profileKey}) {
+    const {clientToken} = await seneca.act({
+      role:'gateway',
+      cmd:'generateClientToken',
+    })
+    const {key} = await seneca.act({
+      role: 'Firebase',
+      cmd: 'push',
+      model: 'Engagements',
+      values: {
+        oppKey,
+        profileKey,
+        isApplied: true,
+        isAccepted: false,
+        isConfirmed: false,
+        paymentClientToken: clientToken,        
+      },
+    })
+    await seneca.act({
+      role: ROLE,
+      cmd: 'notify',
+      templateId: '96e36ab7-43b0-4d45-8309-32c52530bd8a',
+      subjectTemplate: 'Sit Tight! -project_name- has your application.',
+      key,
+    })
+    return {key}
+  }
+}
+
+function remove({seneca}) {
+  return async function({key}) {
+    const {assignments} = await seneca.act({
+      role: 'Firebase',
+      cmd: 'get',
+      assignments: {engagementKey: key},
+    })
+
+    const assignmentKeys = assignments.map(prop('$key'))
+    const shiftKeys = uniq(assignments.map(prop('shiftKey')))
+
+    await Promise.all(assignmentKeys.map(key => seneca.act({
+      role: 'Firebase',
+      cmd: 'remove',
+      model: 'Assignments',
+      key,
+    })))
+    await Promise.all(shiftKeys.map(key => seneca.act({
+      role: 'Shifts',
+      cmd: 'updateCounts',
+      key,
+    })))
+    await seneca.act({
+      role: 'Firebase',
+      cmd: 'remove',
+      model: 'Engagements',
+      key,
+    })
+    return {key}
+  }
+}
+
+
+function notify({seneca, domain}) {
+  return async function({key, templateId, subjectTemplate}) {
+    const {engagement, profile, opp, project} = await seneca.act({
+      role: 'Firebase',
+      cmd: 'get',
+      engagement: key,
+      profile: ['engagement', 'profileKey'],
+      opp: ['engagement', 'oppKey'],
+      project: ['opp', 'projectKey'],
+    })
+    const substitutions = {
+      '-username-': profile.fullName,
+      '-opp_name-': opp.name,
+      '-project_name-': project.name,
+      '-engagementurl-': `${domain}/engaged/${key}/`,
+    }
+    const subject = Object.keys(substitutions).reduce((a,x) => a.replace(x, substitutions[x]), subjectTemplate)
+    await seneca.act({
+      role: 'sendgrid',
+      cmd: 'sendTemplate',
+      templateId,
+      subject,
+      recipient: profile.email,
+      sender: 'help@sparks.network',
+      substitutions,        
+    })
+  }
+}
